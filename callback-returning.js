3575d3bbf648b515201a5ea3394a3fbeed880154
Kelsy Watkins <klwatkins132@gmail.com>
2022-07-27 17:28:24 -0600

    add callback functions section

---
+++ callback-returning.js
@@ -0,0 +1,30 @@
+'use strict';
+///////////////////////////////////////
+// Functions Accepting Callback Functions
+
+console.log('*****Functions Accepting Callback Functions*****');
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase(); // returns a string with no spaces in it
+};
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' '); // spread into an array - rest pattern
+    return [first.toUpperCase(), ...others].join(' '); // destructure 
+};
+// Higher order function
+const transformer = function (str, fn = () => { }) {
+    console.log(`Original string: ${str}`)
+    console.log(`Transformed string: ${fn(str)}`)
+
+    console.log(`Transformed by: ${fn.name}`)
+};
+// Pass CallBack functions
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+    console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
