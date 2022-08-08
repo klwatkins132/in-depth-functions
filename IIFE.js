08b366adf7664485747ed5fff0eda2b19bb10db6
Kelsy Watkins <klwatkins132@gmail.com>
2022-08-08 12:40:09 -0600

    testing IIFE

---
+++ IIFE.js
@@ -0,0 +1,27 @@
+///////////////////////////////////////
+// Immediately Invoked Function Expressions (IIFE)
+
+const runOnce = function () {
+    console.log('This will never run again')
+};
+runOnce();
+
+// IIFE Pattern
+(function () {
+    console.log('This will never run again')
+    const isPrivate = 23;
+})();
+
+//console.log(isPrivate); // ReferenceError - GlobalScope does not have access to function scope
+
+(() => (console.log('This will ALSO never run again')))();
+
+// if you want to create a new scope for data privacy just create a block like below
+// you don't need to make a new function to create a new scope
+{
+    const isPrivate = 23;
+    var notPrivate = 46; // Remember var essentialy ignores the block
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
